248aa0597453dd1e567bc098e4506de1a080244d
Ishwar Suthar <90741501+Ishwar608@users.noreply.github.com>
2024-04-09 21:44:21 +0530

    static image upload done

---
+++ DynamicJson/static-image.ts
@@ -0,0 +1,27 @@
+import image1 from "../public/images/sample-3.jpg";
+import image2 from "../public/images/sample-4.jpg";
+import image3 from "../public/images/sample-5.jpg";
+import image4 from "../public/images/sample-6.jpg";
+
+export const ImageService = [
+  {
+    id: 1,
+    name: "Google",
+    url: image1,
+  },
+  {
+    id: 2,
+    name: "Google1",
+    url: image2,
+  },
+  {
+    id: 3,
+    name: "Google2",
+    url: image3,
+  },
+  {
+    id: 4,
+    name: "Google3",
+    url: image4,
+  },
+];
